8f2c51a5a9aaa89ec0dd2c9f91581336203fb94b
Mario Aguilar Alves <maguilar@hoyvoy.com>
2018-05-30 18:34:38 +0000

    Apply fixes from StyleCI

--- Modules/User/Tests/Feature/LoginTest.php
+++ Modules/User/Tests/Feature/LoginTest.php
@@ -38,13 +38,13 @@ class LoginTest extends TestCase
     public function testLoginsUnsuccessfully()
     {
         $client = OauthClient::create([
-            'id' => config('auth.auth.api_client_id'),
-            'name' => config('app.name').' Password Grant Client',
-            'secret' => config('auth.auth.api_client_secret'),
-            'redirect' => config('app.url'),
+            'id'                     => config('auth.auth.api_client_id'),
+            'name'                   => config('app.name').' Password Grant Client',
+            'secret'                 => config('auth.auth.api_client_secret'),
+            'redirect'               => config('app.url'),
             'personal_access_client' => 0,
-            'password_client' => 1,
-            'revoked' => 0
+            'password_client'        => 1,
+            'revoked'                => 0,
         ]);
 
         $user = factory(User::class)->create([
